0664ddac0c873818846693298265649e465f9f51
Arash Pamazari <87909442+ArashPamazari@users.noreply.github.com>
2023-08-12 09:53:08 +0330

    6.Gradiant.py

--- 6.Gradiant.py
+++ 6.Gradiant.py
@@ -4,19 +4,18 @@ import numpy as np
 
 image_size = 800
 
-# ایجاد تصویر مربعی سفید
+
 image = np.ones((image_size, image_size, 3), dtype=np.uint8) * 255
 
-# تغییر رنگ سطر به سطر از بالا به سمت مشکی
+
 for row in range(image_size):
-    intensity = int(row / image_size * 255)  # محاسبه مقدار شدت رنگ مشکی بر اساس سطر
+    intensity = int(row / image_size * 255) 
     image[row, :, :] = (intensity, intensity, intensity)
 
 for row in range(image_size):
-    intensity = int((image_size - row) / image_size * 255)  # محاسبه مقدار شدت رنگ سفید بر اساس سطر
+    intensity = int((image_size - row) / image_size * 255)  
     image[row, :, :] = (intensity, intensity, intensity)
 
-# نمایش تصویر
 cv2.imwrite('result.jpg',image)
 
-cv2.destroyAllWindows()
+
